5fd0fa665def3bb7b366cfb82c851f88bb581ab0
Jasonzss <2418972236@qq.com>
2023-11-28 16:42:44 +0800

    封装axios

--- src/utils/http.js
+++ src/utils/http.js
@@ -8,14 +8,25 @@ const httpInstance = axios.create({
 })
 
 // axios请求拦截器
-httpInstance.interceptors.request.use(config => {
-    return config
-}, e => Promise.reject(e))
+httpInstance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
 
 // axios响应拦截器
-httpInstance.interceptors.response.use(res => res.data, e => {
-    return Promise.reject(e)
-})
+httpInstance.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
 
+  
 //导出axios实例
 export default httpInstance
